4b31499260f0423d19c9838f9c02b1c6745f0391
meggs1 <megan.schertz@gmail.com>
2020-11-29 15:29:16 -0600

    commit tree 8e43a4843afc12fc918eef57a30a77f56c1b0ed3

---
+++ lib/board.rb
@@ -0,0 +1,50 @@
+require 'pry'
+class Board
+    attr_accessor :cells
+
+def initialize
+    @cells = cells || Array.new(9, " ")
+end
+
+def reset!
+    cells.clear
+    self.cells = Array.new(9, " ")
+end
+
+def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+end
+
+def position(input)
+    cells[input.to_i - 1]
+end
+
+def full?
+    if cells.any?(" ")
+        false
+    else cells.include?("X" || "O")
+        true
+    end
+end
+
+def turn_count
+    cells.count{|token| token == "X" || token == "O"}
+end
+
+def taken?(input)
+    self.position(input) == "X" || self.position(input) == "O"
+end
+
+def valid_move?(input)
+    input.to_i.between?(1,9) && taken?(input) == false
+end
+
+def update(input, player)
+    cells[input.to_i - 1] = player.token
+end
+
+end
---
+++ lib/players/computer.rb
@@ -0,0 +1,23 @@
+module Players
+    class Computer < Player
+
+        CORNERS = [0, 2, 6, 8]
+
+        def move(board)
+            move = nil
+
+            if !board.taken?("5")
+             move = "5"
+            elsif board.turn_count == 1
+              move = "1"
+            elsif board.turn_count == 2
+              move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+            elsif board.turn_count == 3 && (board.position(1) == board.position(9) && board.position(1) !=" " || board.position(3) == board.position(7) && board.position(3) !=" ") 
+              move = "2"
+            else
+              move = [1, 3 ,7 ,9 ,2 ,4 ,6 ,8].detect{ |i| !board.taken?(i)}.to_s if move == nil
+            end
+            move
+        end
+    end
+end
